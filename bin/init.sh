6ec6afad44a4cb898595284094a9ed88fd6281df
Xi Matriarx <xi@ximatriarx.io>
2024-02-14 17:09:18 +0200

    chunkloading (#14)

--- bin/init.sh
+++ bin/init.sh
@@ -8,7 +8,7 @@ then
 	exit
 fi
 
-git clone https://github.com/XiMatriarx/react.git --depth 1 --branch 'v0.0.5'
+git clone https://github.com/XiMatriarx/react.git --depth 1 --branch 'v0.0.6'
 mv react $project
 cd $project
 rm -Rf .git
