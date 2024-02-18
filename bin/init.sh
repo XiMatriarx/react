721fdcdbbf9103826188e2b3c900437e7dcecac7
Xi Matriarx <xi@ximatriarx.io>
2024-02-18 13:59:32 +0200

    version

--- bin/init.sh
+++ bin/init.sh
@@ -8,7 +8,7 @@ then
 	exit
 fi
 
-git clone https://github.com/XiMatriarx/react.git --depth 1 --branch 'v1.0.16'
+git clone https://github.com/XiMatriarx/react.git --depth 1 --branch 'v2.0.0'
 mv react $project
 cd $project
 rm -Rf .git
